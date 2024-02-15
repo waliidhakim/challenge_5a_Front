06da28ba5ca911857a8e26e0cd1b40822d42a342
walid IDIR <idirwalidhakim31@gmail.com>
2024-02-15 19:04:27 +0100

    updating the front

---
+++ components/Footer/Footer.js
@@ -0,0 +1,18 @@
+// components/Footer.js
+import React from 'react';
+import styles from './Footer.module.css'; // Assurez-vous de créer un fichier CSS correspondant
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <p>© 2024 VotreEntreprise. Tous droits réservés.</p>
+        <a href="#">Mentions légales</a>
+        <a href="#">Politique de confidentialité</a>
+        <a href="#">Contact</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
